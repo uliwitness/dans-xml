e250e9830947aaccd4c853041a603ad4c5bd004b
Uli Kusterer <ulivc@zathras.de>
2016-05-28 03:04:21 +0200

    Initial Commit

---
+++ dans_xml/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dans_xml
+//
+//  Created by Uli Kusterer on 28/05/16.
+//  Copyright © 2016 Uli Kusterer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
